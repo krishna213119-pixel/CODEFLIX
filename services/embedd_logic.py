6801db21bbd75660bf4ee309ea492f13c12c0f48
krishna213119-pixel <krishna213119@gmail.com>
2026-07-30 00:48:04 +0530

    Use Hugging Face API embeddings

--- services/embedd_logic.py
+++ services/embedd_logic.py
@@ -1,25 +1,33 @@
+import os
+
 from langchain_core.documents import Document
 from langchain_chroma import Chroma
-from langchain_huggingface import HuggingFaceEmbeddings
-
-
+from langchain_huggingface import HuggingFaceEndpointEmbeddings
 
 
 class EmbeddingService:
 
-    embedding_model = HuggingFaceEmbeddings(
-        model_name="sentence-transformers/all-MiniLM-L6-v2"
-    )
+    @classmethod
+    def get_embedding_model(cls):
+
+        return HuggingFaceEndpointEmbeddings(
+            model="sentence-transformers/all-MiniLM-L6-v2",
+            huggingfacehub_api_token=os.getenv(
+                "HUGGINGFACEHUB_API_TOKEN"
+            )
+        )
 
     @classmethod
     def create_vectorstore(
         cls,
         chunks: list[Document]
-    ) :
+    ):
+
+        embedding_model = cls.get_embedding_model()
 
         vectorstore = Chroma.from_documents(
             documents=chunks,
-            embedding=cls.embedding_model,
+            embedding=embedding_model,
             persist_directory="./vectorstore"
         )
 
