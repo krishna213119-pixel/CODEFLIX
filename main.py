1adc74234ce2d1c06e74768e95de08bdf85a84a3
krishna213119-pixel <krishna213119@gmail.com>
2026-07-30 00:16:04 +0530

    test

--- main.py
+++ main.py
@@ -1,44 +1,54 @@
+# from fastapi import FastAPI
+
+# from api.routes.index import (
+#     router as index_router
+# )
+
+# from api.routes.chat import (
+#     router as chat_router
+# )
+
+
+# app = FastAPI(
+#     title="AI Software Engineer",
+#     description="GitHub Repository AI Assistant",
+#     version="1.0.0"
+# )
+
+
+# app.include_router(
+#     index_router
+# )
+
+# app.include_router(
+#     chat_router
+# )
+
+
+# @app.get("/")
+# def home():
+
+#     return {
+#         "message": (
+#             "AI Software Engineer API "
+#             "is running"
+#         )
+#     }
+
+
+# @app.get("/health")
+# def health():
+
+#     return {
+#         "status": "healthy"
+#     }
 from fastapi import FastAPI
 
-from api.routes.index import (
-    router as index_router
-)
-
-from api.routes.chat import (
-    router as chat_router
-)
-
-
-app = FastAPI(
-    title="AI Software Engineer",
-    description="GitHub Repository AI Assistant",
-    version="1.0.0"
-)
-
-
-app.include_router(
-    index_router
-)
-
-app.include_router(
-    chat_router
-)
+app = FastAPI()
 
 
 @app.get("/")
 def home():
-
     return {
-        "message": (
-            "AI Software Engineer API "
-            "is running"
-        )
-    }
-
-
-@app.get("/health")
-def health():
-
-    return {
-        "status": "healthy"
+        "message": "CODEFLIX backend is running"
     }
