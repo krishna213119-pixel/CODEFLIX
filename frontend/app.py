03e23ac4f993d5f7e51b46a632975383597ce587
krishna213119-pixel <krishna213119@gmail.com>
2026-07-30 01:01:22 +0530

    Update Streamlit frontend

--- frontend/app.py
+++ frontend/app.py
@@ -6,7 +6,7 @@ import requests
 # CONFIG
 # =====================================
 
-API_URL = "http://127.0.0.1:8000"
+API_URL = "https://codeflix-backend-rvij.onrender.com"
 
 
 st.set_page_config(
